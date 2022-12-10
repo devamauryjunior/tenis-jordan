e0082c8cacca05972fa1ea0fc07b816d3b39d70f
devamauryjunior <jobagpj@gmail.com>
2022-12-10 20:46:40 -0300

    feat: add caminho das imagens

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,25 @@
 import React, {useState, useEffect} from "react";
 import Shoes from "../../components/Shoes/Shoes";
 import './style.css';
+import imagen1 from './../../assets/img/shoe1.svg';
+import imagen2 from './../../assets/img/shoe2.svg';
+import imagen3 from './../../assets/img/shoe3.svg';
+import imagen4 from './../../assets/img/shoe4.svg';
+import imagen5 from './../../assets/img/shoe5.svg';
+import imagen6 from './../../assets/img/shoe6.svg';
+import imagen7 from './../../assets/img/shoe7.svg';
 
 function Home() {
     const [arrImages, setImagesArray] = useState([]);
     useEffect(() => {
         setImagesArray([
-            "src/assets/img/shoe1.svg",
-            "src/assets/img/shoe2.svg",
-            "src/assets/img/shoe3.svg",
-            "src/assets/img/shoe4.svg",
-            "src/assets/img/shoe5.svg",
-            "src/assets/img/shoe6.svg",
-            "src/assets/img/shoe7.svg",
+            imagen1,
+            imagen2,
+            imagen3,
+            imagen4,
+            imagen5,
+            imagen6,
+            imagen7,
         ])
     }, [])
     
